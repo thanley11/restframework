f629cc24849d9c28e72d307ebd17071f7de87e39
Tom Hanley <thanley11@gmail.com>
2014-03-02 18:54:03 -0500

    updated urls, views, serializers

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,33 @@
+from rest_framework import serializers
+from snippets.models import Snippet
+from django.contrib.auth.models import User
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.Field(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(view_name='snippet-detail', many=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'snippets')
+
+
+"""
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.Serializer):
+    
     pk = serializers.Field()
     title = serializers.CharField(required=False, max_length = 100)
     code = serializers.CharField(widget=widgets.Textarea,max_length=100000)
@@ -11,13 +36,13 @@ class SnippetSerializer(serializers.Serializer):
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default = 'friendly')
     
     def restore_object(self, attrs, instance=None):
-        """
+        
         Create or update a new snippet instance, given a dictionary
         of deserialized field values.
 
         Note that if we don't define this method, then deserializing
         data will simply return a dictionary of items.
-        """
+        
         if instance:
             # Update existing instance
             instance.title = attrs.get('title', instance.title)
@@ -29,3 +54,8 @@ class SnippetSerializer(serializers.Serializer):
 
         # Create new instance
         return Snippet(**attrs)
+        
+    class Meta:
+        model = Snippet
+        fields = ('id','title','code','linenos','language','style')
+        """
