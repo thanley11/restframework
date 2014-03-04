e24542bad670c6b9974cb9f33a7f1b72d4908315
Tom Hanley <thanley11@gmail.com>
2014-03-04 18:27:56 -0500

    Use ViewSets, use Routers in urls.py, added backups to show previous methods before refactoring

--- snippets/views.py
+++ snippets/views.py
@@ -2,11 +2,42 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.decorators import api_view, link
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from snippets.permissions import IsOwnerOrReadOnly
 
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, 
+                          IsOwnerOrReadOnly,)
+                          
+    @link(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+        
+    def pre_save(self, obj):
+        obj.owner = self.request.user
+    
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+"""   
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    }
+)
+      ^Not needed after using routers, because The DefaultRouter class we're 
+      using also automatically creates the API root view for us
+
+Add viewsets above
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -22,22 +53,6 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     
     def pre_save(self, obj):
         obj.owner = self.request.user
-            
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    }
-    )
         
 class SnippetHighlight(generics.GenericAPIView):
      queryset = Snippet.objects.all()
@@ -46,3 +61,12 @@ class SnippetHighlight(generics.GenericAPIView):
      def get(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
+        
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+"""    
